031eb83c4024c245038d1db336001928963a53e3
Nikolas Thitu <nikolas@hauz.io>
2019-12-19 15:11:22 -0500

    verify jwt

--- includes/jwt-functions.php
+++ includes/jwt-functions.php
@@ -17,14 +17,14 @@ const UAT_WELL_KNOWN_URL  = 'https://gateway.staging.trusona.net/oidc/.well-know
 const PROD_WELL_KNOWN_URL = 'https://gateway.trusona.net/oidc/.well-known/openid-configuration';
 const ALGORITHMS = array('RS256');
 
-function is_valid($jwt, $production = true)
+function is_valid_jwt($jwt, $production = true)
 {
   try {
     $url = $production == true ? PROD_WELL_KNOWN_URL : UAT_WELL_KNOWN_URL;
-    $jwk_arr = (array)json_decode(jwk_set_json($url));
+    $jwk_arr = (array)json_decode(__jwk_set_json($url));
 
     foreach ($jwk_arr['keys'] as $key) {
-      $result = is_valid_jwt($key, $jwt);
+      $result = __is_valid_jwt($key, $jwt);
 
       if($result) {
         return true;
@@ -38,7 +38,7 @@ function is_valid($jwt, $production = true)
   return false;
 }
 
-function is_valid_jwt($key, $jwt)
+function __is_valid_jwt($key, $jwt)
 {
   try {
     $pem = (new JWKConverter())->toPEM((array)$key);
@@ -50,7 +50,7 @@ function is_valid_jwt($key, $jwt)
   }
 }
 
-function jwk_set_json($url)
+function __jwk_set_json($url)
 {
   $json = json_decode(file_get_contents($url));
   return file_get_contents($json->jwks_uri);
