01b44957f0d12db2fce5bd0e0989e763da2dcca9
Nikolas Thitu <nikolas@hauz.io>
2019-12-19 22:09:39 -0500

    only doing HS256/HS512 verification; updating tests

--- includes/jwt-functions.php
+++ includes/jwt-functions.php
@@ -1,59 +1,60 @@
 <?php
-//
-//
-// JWT dependencies:
-// -----------------
-//   1. https://github.com/acodercat/php-jwk-to-pem
-//      > composer require codercat/jwk-to-pem
-//   2. https://github.com/firebase/php-jwt
-//      > composer require firebase/php-jwt
-//
-require_once 'vendor/autoload.php';
 
-use CoderCat\JWKToPEM\JWKConverter;
-use Firebase\JWT\JWT;
+const SHA_256 = 'sha256';
+const SHA_512 = 'sha512';
+const HS_256 = 'HS256';
+const HS_512 = 'HS512';
 
-const UAT_WELL_KNOWN_URL  = 'https://gateway.staging.trusona.net/oidc/.well-known/openid-configuration';
-const PROD_WELL_KNOWN_URL = 'https://gateway.trusona.net/oidc/.well-known/openid-configuration';
-const ALGORITHMS = array('RS256');
+function base64url_encode($data)
+{
+  $b64 = base64_encode($data);
+  $url = strtr($b64, '+/', '-_');
+  return rtrim($url, '=');
+}
 
-function is_valid_jwt($jwt, $production = true)
+function base64url_decode($data, $strict = false)
+{
+  $b64 = strtr($data, '-_', '+/');
+  return base64_decode($b64, $strict);
+}
+
+function extract_algorithm($header) {
+  $obj = json_decode(base64url_decode($header));
+
+  if($obj->alg === HS_256) {
+    return SHA_256;
+  }
+  else if($obj->alg === HS_512) {
+    return SHA_512;
+  }
+
+  return NULL;
+}
+
+function not_expired($payload) {
+  $obj = json_decode(base64url_decode($payload));
+  return time() < $obj->exp;
+}
+
+function issued_in_past($payload) {
+  $obj = json_decode(base64url_decode($payload));
+  return time() >= $obj->iat;
+}
+
+function is_valid_jwt($token, $secret)
 {
   try {
-    $url = $production == true ? PROD_WELL_KNOWN_URL : UAT_WELL_KNOWN_URL;
-    $jwk_arr = (array)json_decode(__jwk_set_json($url));
+    $arr = explode('.', $token, 3);
+    $algorithm = extract_algorithm($arr[0]);
+    $signature = base64url_encode(hash_hmac($algorithm, "{$arr[0]}.{$arr[1]}", $secret,true));
 
-    foreach ($jwk_arr['keys'] as $key) {
-      $result = __is_valid_jwt($key, $jwt);
-
-      if($result) {
-        return true;
-      }
-    }
+    return $signature === $arr[2] 
+      && not_expired($arr[1]) 
+      && issued_in_past($arr[1]);
   }
   catch(Exception $e) {
     return false;
   }
-
-  return false;
-}
-
-function __is_valid_jwt($key, $jwt)
-{
-  try {
-    $pem = (new JWKConverter())->toPEM((array)$key);
-    $decoded = JWT::decode($jwt, $pem, ALGORITHMS);
-    return isset($decoded);
-  }
-  catch(Exception $e) {
-    return false;
-  }
-}
-
-function __jwk_set_json($url)
-{
-  $json = json_decode(file_get_contents($url));
-  return file_get_contents($json->jwks_uri);
 }
 
 ?>
