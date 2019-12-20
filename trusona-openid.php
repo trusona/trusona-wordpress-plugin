1fc2ca5268174651bc012805d0cda5234cccf7b4
Nikolas Thitu <nikolas@hauz.io>
2019-12-19 22:10:03 -0500

    tying id_token verification

--- trusona-openid.php
+++ trusona-openid.php
@@ -4,7 +4,7 @@
     Plugin Name: Trusona
     Plugin URI: https://wordpress.org/plugins/trusona/
     Description: Login to your WordPress with Trusona’s FREE #NoPasswords plugin. This plugin requires the Trusona app. View details for installation instructions.
-    Version: 2.0.0
+    Version: 1.5.0
     Author: Trusona
     Author URI: https://trusona.com
     License: MIT
@@ -12,8 +12,8 @@
 
     defined('ABSPATH') or die();
 
-    require_once 'phar://' . plugin_dir_path( __FILE__ ) . 'phar/trusona-includes.phar.gz/trusona-functions.php';
-    require_once 'phar://' . plugin_dir_path( __FILE__ ) . 'phar/trusona-includes.phar.gz/jwt-functions.php';
+    require_once plugin_dir_path( __FILE__ ) . 'includes/trusona-functions.php';
+    require_once plugin_dir_path( __FILE__ ) . 'includes/jwt-functions.php';
 
 class TrusonaOpenID
 {
@@ -265,11 +265,11 @@ class TrusonaOpenID
         $token_response = json_decode($token_result['body'], true);
         $authenticated  = false;
 
+        $secret = get_option(self::PLUGIN_ID_PREFIX . 'client_secret', null);
         $access_token = $token_response['access_token'];
         $id_token = $token_response['id_token'];
-        $env = is_production(self::LOGIN_URL);
 
-        if (isset($token_response['token_type'], $access_token) && is_valid_jwt($access_token, $env)) {
+        if (isset($token_response['token_type'], $access_token)) {
             $authorization = "{$token_response['token_type']} {$access_token}";
             $headers       = array('Authorization' => $authorization);
 
@@ -280,7 +280,7 @@ class TrusonaOpenID
                 $this->error_redirect(3);
                 return;
             }
-        } elseif (isset($id_token) && is_valid_jwt($id_token, $env)) {
+        } elseif (isset($id_token) && is_valid_jwt($id_token, $secret)) {
             $jwt_arr    = explode('.', $id_token);
             $user_claim = json_decode(base64_decode($jwt_arr[1]), true);
         } else {
